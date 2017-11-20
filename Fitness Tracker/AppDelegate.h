633bb33a1cb1765b6e5acbf3b7c1bc460d367b0b
Matthew Fisher [el16m2f] <el16m2f@eentpc246.leeds.ac.uk>
2017-11-20 12:32:42 +0000

    Initial Commit

---
+++ Fitness Tracker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Fitness Tracker
+//
+//  Created by Matthew Fisher [el16m2f] on 20/11/2017.
+//  Copyright © 2017 Matthew Fisher [el16m2f]. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Fitness Tracker/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Fitness Tracker
+//
+//  Created by Matthew Fisher [el16m2f] on 20/11/2017.
+//  Copyright © 2017 Matthew Fisher [el16m2f]. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
